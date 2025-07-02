a82d122f36b080bb4405a89ca7d76440bf3188ab
LeonardoGameshow <leonciogamershow@gmail.com>
2025-07-01 23:56:04 -0600

    complete 1st slider -LGS

--- core/views.py
+++ core/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    logos = [
+        {'src': 'img/one-piece.png', 'class': ''},
+        {'src': 'img/html-5.png', 'class': ''},
+        {'src': 'img/css-3.png', 'class': ''},
+        {'src': 'img/javascript.png', 'class': ''},
+        {'src': 'img/bootstrap.png', 'class': ''},
+        {'src': 'img/django.png', 'class': ''},
+        {'src': 'img/git.png', 'class': ''},
+        {'src': 'img/mysql.png', 'class': ''},
+        {'src': 'img/python.png', 'class': ''},
+        {'src': 'img/php.png', 'class': ''},
+        {'src': 'img/java.png', 'class': ''},
+        {'src': 'img/laravel.png', 'class': ''},
+        {'src': 'img/react.png', 'class': ''},
+        {'src': 'img/swift.png', 'class': 'swift'},
+        {'src': 'img/android_studio.png', 'class': 'android'},
+        {'src': 'img/wordpress.png', 'class': 'wordpress'},
+    ]
+    return render(request, 'index.html', {'logos': logos})
