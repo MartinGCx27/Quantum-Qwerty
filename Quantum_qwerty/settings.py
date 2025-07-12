2fc7d1ee407fcaa6b9f65a2908c52a925202cdd7
LeonardoGameshow <leonciogamershow@gmail.com>
2025-07-12 10:38:02 -0600

    contact us style -LGS

--- Quantum_qwerty/settings.py
+++ Quantum_qwerty/settings.py
@@ -145,3 +145,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Se agregan nuevos captcha para QUANTUM QWERTY -LGS
+RECAPTCHA_PUBLIC_KEY = ''
+RECAPTCHA_SECRET_KEY = ''
