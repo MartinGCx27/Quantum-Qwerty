525d04b1074ddbf954f5453facd9efe3ca65ce00
MartinGCx27 <martingcx27@gmail.com>
2025-07-25 07:17:14 +0000

    Fix CSRF Token to deploy  contact form -Zox

--- Quantum_qwerty/settings.py
+++ Quantum_qwerty/settings.py
@@ -168,6 +168,8 @@ BASE_URL = os.environ.get('BASE_URL', 'http://localhost:8000')
 
 RECIPIENT_LIST = [os.environ.get("RECIPIENT_EMAIL")]
 
-CSRF_TRUSTED_ORIGINS = ['https://quantumqwerty.com/'] 
+CSRF_TRUSTED_ORIGINS = ['https://quantumqwerty.com'] 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = False
+
