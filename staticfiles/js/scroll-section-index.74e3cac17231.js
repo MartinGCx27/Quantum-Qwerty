685f331163f3cfb360535c444a8af7d6c187f004
MartinGCx27 <martingcx27@gmail.com>
2025-09-22 18:29:52 +0000

    push from server -Zox

--- staticfiles/js/scroll-section-index.74e3cac17231.js
+++
@@ -1,3 +0,0 @@
-import { animationsScroll } from './qq-functions.js';
-
-  animationsScroll.scrollToSecondSection('.hyperjump-effect', '#about-us', 6500);
--- staticfiles/js/scroll-section-services.8f9b3a8168bc.js
+++
@@ -1,3 +0,0 @@
-import { animationsScroll } from './qq-functions.js';
-
-  animationsScroll.scrollToSecondSection('.animation-services', '.content-services', 4000);
